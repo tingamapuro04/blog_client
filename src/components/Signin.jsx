9b82e0d069502a6f51957ccb719926e172e36a40
tingamapuro04 <alponcemobutu100@gmail.com>
2023-09-14 19:49:00 +0300

    sign in page

---
+++ src/components/Signin.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SignIn() {
+  const navigate = useNavigate();
+
+  // State to store user input
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send user data to the server using the fetch API for sign-in
+    fetch("/api/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Successful sign-in, you can navigate to the user dashboard or handle it as needed
+          navigate("/dashboard");
+        } else {
+          // Handle sign-in errors
+          console.error("Sign-in failed");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign In
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 rounded-t-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                value={userData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 rounded-b-md placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={userData.password}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+        <div className="text-center">
+          <p className="text-sm text-gray-600">
+            Don't have an account?{" "}
+            <span
+              onClick={() => navigate("/signup")}
+              className="cursor-pointer text-indigo-600 hover:text-indigo-800"
+            >
+              Sign Up
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import NavBar from './components/Header.jsx'
 import Post from './components/Post.jsx';
 import Posts from './components/Posts.jsx';
 import Signup from './components/Signup.jsx';
+import SignIn from './components/Signin.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <NavBar />
     <Routes>
-      <Route path="/signup" element={ <Signup /> }></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/signin" element={<SignIn />}></Route>
       <Route path="/" element={<App />}></Route>
       <Route path="/:user_id/posts/:post_id" element={<Post />}></Route>
       <Route path="/:user_id/posts" element={<Posts />}></Route>
