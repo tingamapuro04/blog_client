2d553dbdcd3c56ccd9586a961fd1ec9b27429100
tingamapuro04 <alponcemobutu100@gmail.com>
2023-09-14 19:10:11 +0300

    post and posts components

---
+++ src/components/Post.jsx
@@ -0,0 +1,9 @@
+const Post = () => {
+  return (
+    <div>
+      This is for a single post
+    </div>
+  )
+}
+
+export default Post
---
+++ src/components/Posts.jsx
@@ -0,0 +1,9 @@
+const Posts = () => {
+  return (
+    <div>
+      This will have a list of all the posts
+    </div>
+  )
+}
+
+export default Posts;
