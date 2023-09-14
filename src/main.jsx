06e590fc253a964421a4c21c1d5763cfc4e69019
tingamapuro04 <alponcemobutu100@gmail.com>
2023-09-14 19:01:34 +0300

    Browser router for react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 import NavBar from './components/Header.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <NavBar />
-    <App />
-  </React.StrictMode>,
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+    </Routes>
+  </Router>,
 )
