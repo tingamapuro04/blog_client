cd8382cc7bba63eb4f287c6e63aba24485f789ca
tingamapuro04 <alponcemobutu100@gmail.com>
2023-09-15 08:34:29 +0300

    axios

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react'
+
+function Home() {
+  const [posts, setPosts ] = useState([]);
+  useEffect(() => {
+    
+  }, [])
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
