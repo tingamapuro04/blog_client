c1c1a805264dc0f38bd5644dbcf657d24be1fb52
tingamapuro04 <alponcemobutu100@gmail.com>
2023-09-24 13:25:02 +0300

    working navigation bar

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from './App.jsx'
+import { ThemeProvider } from "@material-tailwind/react";
 import './index.css'
-import NavBar from './components/Header.jsx'
+import { Header } from './components/Header.jsx'
 import Post from './components/Post.jsx';
 import Posts from './components/Posts.jsx';
 import Signup from './components/Signup.jsx';
@@ -13,14 +14,14 @@ import CreatePost from './components/CreatePost.jsx';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <Router>
-      <NavBar />
+      <Header />
       <Routes>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/signin" element={<SignIn />}></Route>
         <Route path="/" element={<Home />}></Route>
         <Route path="/posts/:post_id" element={<Post />}></Route>
         <Route path="/posts" element={<Posts />}></Route>
-        <Route path="/posts/write" element={ <CreatePost /> }></Route>
+        <Route path="/posts/write" element={<CreatePost />}></Route>
       </Routes>
     </Router>
   </>
